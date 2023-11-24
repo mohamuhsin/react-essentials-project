014d961945c5d2ae30296f22099ec48482d3032b
mohamuhsin <muhsinohoho@outlook.com>
2023-11-24 23:37:51 +0300

    rendered content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,20 @@ function App() {
     setSelectedTopic(selectedButton);
     console.log("Hello");
   }
+
+  let tabContent = <p>Please select a Topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -41,15 +55,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          }
+          {tabContent}
         </section>
       </main>
     </div>
