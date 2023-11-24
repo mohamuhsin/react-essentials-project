17c0d22373760497cb42f4e5b972593a379465eb
mohamuhsin <muhsinohoho@outlook.com>
2023-11-24 23:00:34 +0300

    created a list of buttons in TabButton.jsx and rendered into the App four times, and used the children prop, to output the content in btn the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from "./components/Header/CoreConcept";
 import Header from "./components/Header/Header";
+import TabButton from "./components/TabButton";
 
 import { CORE_CONCEPTS } from "./data";
 
@@ -20,6 +21,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Jsx</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
