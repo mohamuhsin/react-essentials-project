c73fa423de590f08d58e41e62c1726c0d5b39408
mohamuhsin <muhsinohoho@outlook.com>
2023-11-24 22:49:31 +0300

    created CoreConcept.jsx, created a list of data then used props from our App.jsx and forwaded to the CoreConcept.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import CoreConcept from "./components/Header/CoreConcept";
 import Header from "./components/Header/Header";
+
+import { CORE_CONCEPTS } from "./data";
+
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
